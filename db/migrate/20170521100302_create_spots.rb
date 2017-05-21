75ab36a09ea2f5bde4bf96f1218cfe679ef975d5
Hayato_T <tsunoda.gameobea@x.gmobb.jp>
2017-05-21 21:11:11 +0900

    Import a CSV file to DB

--- db/migrate/20170521100302_create_spots.rb
+++ db/migrate/20170521100302_create_spots.rb
@@ -1,14 +1,13 @@
 class CreateSpots < ActiveRecord::Migration[5.0]
   def change
     create_table :spots do |t|
-      t.string :name-ja
-      t.string :name-en
+      t.string :janame
+      t.string :enname
       t.string :zipcode
-      t.string :address-ja
-      t.sring :address-en
+      t.string :jaaddress
+      t.string :enaddress
       t.float :latitude
       t.float :longitude
-
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+CSV.foreach('db/jta_free_wifi.csv') do |row|
+    Spot.create(:janame => row[1], :enname => row[2], :zipcode => row[6],  :jaaddress => row[8], :enaddress => row[9], :latitude => row[18], :longitude => row[19])
+end
+
