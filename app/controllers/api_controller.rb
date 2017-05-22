07918a1f5e3c20f9c670548edd64616b18a3db1d
Hayato_T <tsunoda.gameobea@x.gmobb.jp>
2017-05-22 16:39:04 +0900

    parameter bug fixed

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -13,7 +13,7 @@ class ApiController < ApplicationController
     if params[:mxnum].blank?
         mx = 5
     else
-        mx = params[:mxnum]
+        mx = params[:mxnum].to_i
     end
     data = Spot.find_by_sql(["SELECT janame,jaaddress,(6371 * 1000 * ACOS(COS(RADIANS(latitude)) * COS(RADIANS(:a)) * COS(RADIANS(longitude) - RADIANS(:b)) + SIN(RADIANS(:a)) * SIN(RADIANS(latitude)))) AS distance FROM spots HAVING distance <= :c ORDER BY distance LIMIT :d",{a: lat,b: lng,c: radius,d: mx}])
     render :json => data
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'api/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get "app/show" => "app#show"
+    get "api/show" => "api#show"
 end
